2950bb87139004d6831dac46918a0ea9cc5d0de8
wisdomyeon <duswl8936@naver.com>
2023-01-28 08:53:15 +0900

    modal 가운데 정렬

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Comon from './page/Comon';
+import Start from './page/Comon';
+
+function App() {
+  return (
+    <div className="App">
+      <Comon></Comon>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/BasicModal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BasicModal = () => {
+    return (
+        <div className='basic_modal'>
+        </div>
+    );
+};
+
+export default BasicModal;
