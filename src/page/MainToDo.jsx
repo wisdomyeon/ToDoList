c67949e67d863647ce8b5fbc78aacf031f445e6b
wisdomyeon <duswl8936@naver.com>
2023-02-01 02:52:58 +0900

    감정상태 modal

--- src/page/MainToDo.jsx
+++ src/page/MainToDo.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { UserInfoRecoil } from './../recoil/recoil';
-import StateIcon from './../page/component/StateIcon';
+import StateButton from './component/StateButton';
+import StateModal from './component/StateModal';
 
 const MainToDo = () => {
     const [userInfo, setUserInfo] = useRecoilState(UserInfoRecoil);
+    const [modal, setModal] = useState(true);
+    const changeModal = () => {
+        setModal(!modal)
+    };
 
     return (
         <div className='basic_modal2'>
             <h1 className='title'>ToDoList</h1>
             <div className='userInfoDiv'>
-                <StateIcon />
+                <StateButton changeModal={changeModal} />
                 <div className='userInfo'>{userInfo.name}</div>
                 <div className='userInfo'>{userInfo.goal}</div>
             </div>
-
+            {modal ? <StateModal /> : null}
         </div >
     );
 };
