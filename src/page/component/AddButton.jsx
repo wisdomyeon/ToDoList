9c5cb829a2670e62b49cca132684c8983a0f8cf4
wisdomyeon <duswl8936@naver.com>
2023-02-09 02:30:04 +0900

    추가버튼 구조

--- src/page/component/AddButton.jsx
+++ src/page/component/AddButton.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const AddButton = () => {
     return (
-        <div className='addBox'>
-            d
-        </div>
+        <>
+            <div className='addBox'>
+                <div className='addButton'>
+                </div>
+            </div>
+        </>
     );
 };
 
